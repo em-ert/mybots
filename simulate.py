7323862e16db68f82a18721a1830831cc51b6d37
Emily Ertle <83237879+em-ert@users.noreply.github.com>
2024-01-17 18:02:27 -0500

    Added comments to code.

--- simulate.py
+++ simulate.py
@@ -1,12 +1,16 @@
 ﻿import pybullet as p
 import time
 
+# Create a physicsClient object and draw the results to a GUI
 physicsClient = p.connect(p.GUI)
+p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
 
 for i in range(1000):
+    # Step the physics simulation then sleep for 1/60th of a second
     p.stepSimulation()
     time.sleep(0.00166)
+    # Print the value of the for loop to the console
     print (i)
 
-
+# Disconnect
 p.disconnect()
