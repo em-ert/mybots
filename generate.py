00ec9c3253793ce6423b3599b4469abbd0541f7c
em-ert <emsterskate@outlook.com>
2024-01-28 14:36:05 -0500

    Slightly modified box stacking program

--- generate.py
+++ generate.py
@@ -10,18 +10,14 @@ y = -2
 #pyrosim.Send_Cube(name="Box2", pos=[1, 0, 1.5], size=[width, length, height])
 
 for g in range (5):
+    for h in range (5):
+        for i in range(10):
+            width_temp = width * (0.9**i)
+            length_temp = length * (0.9**i)
+            height_temp = height * (0.9**i)
+            pyrosim.Send_Cube(name="Box_"+str(i) , pos=[x, y, .5+i], size=[width_temp, length_temp, height_temp])
+        y += 1  
     x += 1
     y = -2
-    for h in range (5):
-        y += 1
-        width = 1
-        height = 1
-        length = 1
-        for i in range(10):
-            width = width*0.9
-            length = length * 0.9
-            height = height * 0.9
-            pyrosim.Send_Cube(name="Box_"+str(i) , pos=[x, y, .5+i], size=[width, length, height])
-
 
 pyrosim.End()
